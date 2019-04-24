0ed97539058697107739a52dcb6f8e4f2c76fe12
wk-j <somnuk.wk@gmail.com>
2019-04-24 10:12:05 +0700

    Added context

--- src/JsonB/Program.cs
+++ src/JsonB/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 
-namespace JsonB
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace JsonB {
+    public class StudentSettings {
+        public int A { set; get; }
+        public int B { set; get; }
+
+    }
+    public class Student {
+        [Key]
+        public int Id { set; get; }
+
+        [Column(TypeName = "jsonb")]
+        public string Settings { set; get; }
+    }
+
+    class MyContext : DbContext {
+        public MyContext(DbContextOptions options) : base(options) {
+
+        }
+
+        public DbSet<Student> Students { set; get; }
+
+    }
+    class Program {
+        static void Main(string[] args) {
+            var connectionString = "Host=localhost;Database=JsonB;User Id=postgres;Password=1234";
+            var options = new DbContextOptionsBuilder().UseNpgsql(connectionString).Options;
+
+            using (var context = new MyContext(options)) {
+                context.Database.EnsureCreated();
+                context.Students.Add(new Student { });
+                context.SaveChanges();
+            }
         }
     }
 }
