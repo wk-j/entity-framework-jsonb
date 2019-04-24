680e2766d707fcc20bb5c79c9e3f870c00f67753
wk-j <somnuk.wk@gmail.com>
2019-04-24 10:51:43 +0700

    Dapper

--- src/JsonB/Program.cs
+++ src/JsonB/Program.cs
@@ -3,11 +3,30 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
+using Npgsql;
+using Dapper;
+using static Dapper.SqlMapper;
+using Newtonsoft.Json.Linq;
+using System.Data;
 
 namespace JsonB {
+    public class JObjectHandler : TypeHandler<JObject> {
+        private JObjectHandler() { }
+        public static JObjectHandler Instance { get; } = new JObjectHandler();
+        public override JObject Parse(object value) {
+            var json = (string)value;
+            return json == null ? null : JObject.Parse(json);
+        }
+        public override void SetValue(IDbDataParameter parameter, JObject value) {
+            parameter.Value = value?.ToString(Newtonsoft.Json.Formatting.None);
+        }
+    }
+
     public class StudentSettings {
         public int A { set; get; }
         public int B { set; get; }
+        public DateTime Min { set; get; } = DateTime.MinValue;
+        public DateTime Max { set; get; } = DateTime.MaxValue;
 
     }
     public class Student {
@@ -24,14 +43,14 @@ namespace JsonB {
         }
 
         public DbSet<Student> Students { set; get; }
-
     }
+
+    class FakeStudent {
+        public JObject Settings { set; get; }
+    }
+
     class Program {
-        static void Main(string[] args) {
-            var connectionString = "Host=localhost;Database=JsonB;User Id=postgres;Password=1234";
-            var options = new DbContextOptionsBuilder().UseNpgsql(connectionString).Options;
-
-
+        static void Save(DbContextOptions options) {
             using (var context = new MyContext(options)) {
                 var settings = new StudentSettings {
                     A = 100,
@@ -45,6 +64,29 @@ namespace JsonB {
                 context.Students.Add(new Student { Settings = json });
                 context.SaveChanges();
             }
+        }
+
+        static void Load(string connectionString) {
+            SqlMapper.AddTypeHandler(JObjectHandler.Instance);
+            using (var conn = new NpgsqlConnection(connectionString)) {
+                conn.Open();
+                var sql = @"select * from ""Students""";
+                var a = conn.QuerySingle<FakeStudent>(sql);
+                var settings = a.Settings.ToObject<StudentSettings>();
+
+                Console.WriteLine(settings.A);
+                Console.WriteLine(settings.B);
+                Console.WriteLine(settings.Min);
+                Console.WriteLine(settings.Max);
+            }
+        }
+
+        static void Main(string[] args) {
+            var connectionString = "Host=localhost;Database=JsonB;User Id=postgres;Password=1234";
+            var options = new DbContextOptionsBuilder().UseNpgsql(connectionString).Options;
+
+            Save(options);
+            Load(connectionString);
         }
     }
 }
